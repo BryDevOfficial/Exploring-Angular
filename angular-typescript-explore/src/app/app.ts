e1fdaf889de5026c744bc3fa9c55ac6d33007eac
BryDev <gransebryan@gmail.com>
2026-01-12 02:31:42 +0800

    fix app.ts to include activebooking test and adjust the container at app.html

--- angular-typescript-explore/src/app/app.ts
+++ angular-typescript-explore/src/app/app.ts
@@ -8,5 +8,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-sandbox');
+  protected readonly title = signal('angular-typescript-explore');
+
+    // 1. You MUST define the variable here for the HTML to see it
+  // 2. We use a Signal because it's the 2026 standard
+  activeBooking = signal({
+    guestName: 'Sea Eagle Guest',
+    roomNumber: 101
+  });
 }
