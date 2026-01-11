3d574790df1521dffab9e2069c00235ac2721a86
BryDev <gransebryan@gmail.com>
2026-01-12 02:12:29 +0800

    added type and interface

--- angular-typescript-explore/src/app/app.component.ts
+++ angular-typescript-explore/src/app/app.component.ts
@@ -1,1 +1,11 @@
 import { Component, signal, computed } from '@angular/core';
+
+type PaymentStatus = 'Paid' | 'Unpaid' | 'Refunded';
+
+interface Booking {
+    id: number;
+    guestName: string;
+    roomNumber: number;
+    status: PaymentStatus;
+    totalPrice: number;
+}
